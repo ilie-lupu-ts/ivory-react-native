fdb0c16308d303957285d62a9a114bd6bdc935a5
ilie-lupu-ts <127083262+ilie-lupu-ts@users.noreply.github.com>
2024-03-01 17:36:48 +0200

    setup android app

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,7 +1,12 @@
 import { Pressable, StyleSheet } from "react-native";
-import { signIn } from "aws-amplify/auth";
+import {
+  signIn,
+  getCurrentUser,
+  signOut,
+  fetchUserAttributes,
+  fetchAuthSession,
+} from "aws-amplify/auth";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 
 export default function TabTwoScreen() {
@@ -13,19 +18,72 @@ export default function TabTwoScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <Pressable onPress={signInHandler}>
-        <Text>Sign in</Text>
-      </Pressable>
+      <View style={styles.buttonsWrapper}>
+        {/* <Text>{authStatus}</Text> */}
+        {/* <Text>isPending: {isPending ? "true" : "false"}</Text> */}
+        <Pressable style={styles.button} onPress={signInHandler}>
+          <Text>Sign in</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={getUserHandler}>
+          <Text>getCurrentUser</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={signOutHandler}>
+          <Text>signOut</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={fetchUserAttributesHandler}>
+          <Text>fetchUserAttributes</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={fetchAuthSessionHandler}>
+          <Text>fetchAuthSession</Text>
+        </Pressable>
+      </View>
     </View>
   );
 
   async function signInHandler() {
     try {
       const user = await signIn({
-        username: "user",
-        password: "password",
+        username: "",
+        password: "",
       });
       console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function getUserHandler() {
+    try {
+      const user = await getCurrentUser();
+      console.log(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function signOutHandler() {
+    try {
+      await signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function fetchUserAttributesHandler() {
+    try {
+      const userAttributes = await fetchUserAttributes();
+      console.log({ userAttributes });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function fetchAuthSessionHandler() {
+    try {
+      const authSession = await fetchAuthSession();
+      const accessToken = authSession.tokens?.accessToken;
+
+      console.log({ accessToken });
     } catch (error) {
       console.log(error);
     }
@@ -46,5 +104,13 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  buttonsWrapper: {
+    gap: 10,
+  },
+  button: {
+    backgroundColor: "grey",
+    padding: 10,
+    borderRadius: 5,
   },
 });
